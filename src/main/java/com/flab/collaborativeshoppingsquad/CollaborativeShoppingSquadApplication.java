551be082efa149782c4bc7e510fd1527fccf83c4
jihunbang <javaprogrammer@naver.com>
2024-01-31 22:33:12 +0900

    프로젝트 초기설정

---
+++ src/main/java/com/flab/collaborativeshoppingsquad/CollaborativeShoppingSquadApplication.java
@@ -0,0 +1,13 @@
+package com.flab.collaborativeshoppingsquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeShoppingSquadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeShoppingSquadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/collaborativeshoppingsquad/CollaborativeShoppingSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.collaborativeshoppingsquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeShoppingSquadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
