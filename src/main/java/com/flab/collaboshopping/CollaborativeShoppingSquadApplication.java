18c47c700b0af215d6d112993e7f82a9ccd2928a
jihunbang <javaprogrammer@naver.com>
2024-01-31 23:27:38 +0900

    멀티모듈 초기구성

--- src/main/java/com/flab/collaboshopping/CollaborativeShoppingSquadApplication.java
+++ src/main/java/com/flab/collaboshopping/CollaborativeShoppingSquadApplication.java
@@ -1,4 +1,4 @@
-package com.flab.collaborativeshoppingsquad;
+package com.flab.collaboshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/flab/collaboshopping/CollaborativeShoppingSquadApplicationTests.java
+++ src/test/java/com/flab/collaboshopping/CollaborativeShoppingSquadApplicationTests.java
@@ -1,4 +1,4 @@
-package com.flab.collaborativeshoppingsquad;
+package com.flab.collaboshopping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
