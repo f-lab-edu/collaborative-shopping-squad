5bc66ac931a1b75ad4d70e9eac2d77c1f36aceef
김선우 <seonwoo960000@toss.im>
2024-02-24 23:27:13 +0900

    Gradle 설정 변경 및 component scan 변경

--- collaboshopping-api/src/main/java/com/flab/collaboshoppingapi/CollaboshoppingApiApplication.java
+++ collaboshopping-api/src/main/java/com/flab/collaboshoppingapi/CollaboshoppingApiApplication.java
@@ -3,11 +3,10 @@ package com.flab.collaboshoppingapi;
 import com.flab.collaboshoppingapp.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration .class})
-@ComponentScan(basePackageClasses = {Application.class})
+@SpringBootApplication
+@ComponentScan(basePackageClasses = {Application.class, CollaboshoppingApiApplication.class})
 public class CollaboshoppingApiApplication {
 
     public static void main(String[] args) {
